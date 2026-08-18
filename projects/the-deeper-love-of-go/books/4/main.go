f8aa42e26fb44d3264726e73f12631d167f836fa
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-08-18 14:46:39 +0700

    re-assigned existing variable value(2)

---
+++ projects/the-deeper-love-of-go/books/4/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var title = "The Mountain is You"
+	var author = "Briana Weist"
+
+	fmt.Println("Books in stock")
+	fmt.Println(title, "by", author)
+
+	title = "Can't Hurt me"
+	author = "David Goggins"
+	fmt.Println(title, "by", author)
+
+}
