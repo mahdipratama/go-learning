4d67d52e0f390a59a7bfbda670b4799e134cd75f
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-08-18 14:13:53 +0700

    re-assigned existing variable value

---
+++ projects/the-deeper-love-of-go/books/3/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var book = "'Master and Commander', by Patrick O'Brian"
+
+	fmt.Println("Books in stock")
+	fmt.Println(book)
+
+	book = "'A Morbid Taste for Bones', by Ellis Peters"
+	fmt.Println(book)
+}
