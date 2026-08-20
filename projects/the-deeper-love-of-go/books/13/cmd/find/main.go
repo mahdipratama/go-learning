458694dff22ad2b5d1057413db9aef716c116a20
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-08-20 15:43:45 +0700

    add: os.Args to ask user prompt about specific id

--- projects/the-deeper-love-of-go/books/13/cmd/find/main.go
+++ projects/the-deeper-love-of-go/books/13/cmd/find/main.go
@@ -3,11 +3,26 @@ package main
 import (
 	"books"
 	"fmt"
+	"os"
 )
 
 func main() {
+	// os.Args is a slice of strings, came from user input as an arguments then store it
+	// os.Args[0]: Contains the name or path of the running program executable.
+	// os.Args[1:]: Contains all the actual user-provided arguments passed to the program.
 
-	book, ok := books.GetBook("ABC03")
+	// HOW TO USE:
+	// Current Folder -> go run main.go [bookId]
+	// root -> go run cmd/find [bookId]
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: find <BOOK ID>")
+		return
+	}
+
+	ID := os.Args[1]
+
+	book, ok := books.GetBook(ID)
 	if !ok {
 		fmt.Println("Sorry, Couldn't find that book in the catalog")
 		return
