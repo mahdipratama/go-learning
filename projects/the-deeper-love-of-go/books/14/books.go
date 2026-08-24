b8c5a9ec6bc4ae36fe1971ebee383f8b8f29445a
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-08-24 09:23:34 +0700

    test unit has data race in this example

---
+++ projects/the-deeper-love-of-go/books/14/books.go
@@ -0,0 +1,48 @@
+package books
+
+import (
+	"fmt"
+	"maps"
+	"slices"
+)
+
+type Book struct {
+	Title  string
+	Author string
+	Copies int
+	ID     string
+}
+
+var catalog = map[string]Book{
+	"ABC03": {
+		Title:  "Never Finished",
+		Author: "David Goggins",
+		Copies: 2,
+		ID:     "ABC03",
+	},
+	"ABC04": {
+		Title:  "The Mountain is You",
+		Author: "Briana Weist",
+		Copies: 1,
+		ID:     "ABC04",
+	},
+}
+
+func GetAllBooks() []Book {
+	return slices.Collect(maps.Values(catalog))
+}
+
+func BookToString(book Book) string {
+	result := fmt.Sprintf("%v by %v (copies: %v)", book.Title, book.Author, book.Copies)
+	return result
+}
+
+func GetBook(ID string) (Book, bool) {
+	book, ok := catalog[ID]
+
+	return book, ok
+}
+
+func AddBook(book Book) {
+	catalog[book.ID] = book
+}
---
+++ projects/the-deeper-love-of-go/books/14/books_test.go
@@ -0,0 +1,89 @@
+package books_test
+
+import (
+	"books"
+	"cmp"
+	"slices"
+	"testing"
+)
+
+func TestGetAllBooks_ReturnsAllBooks(t *testing.T) {
+	want := []books.Book{
+		{
+			Title:  "The Mountain is You",
+			Author: "Briana Weist",
+			Copies: 1,
+			ID:     "ABC04",
+		},
+		{
+			Title:  "Never Finished",
+			Author: "David Goggins",
+			Copies: 2,
+			ID:     "ABC03",
+		},
+	}
+
+	got := books.GetAllBooks()
+	slices.SortFunc(got, func(a, b books.Book) int {
+		return cmp.Compare(a.Author, b.Author)
+	})
+
+	if !slices.Equal(want, got) {
+		t.Fatalf("want %#v, got %#v", want, got)
+	}
+}
+
+func TestGetBook_FindsBookInCatalogByID(t *testing.T) {
+	// Running all the test at the same time
+	t.Parallel()
+
+	want := books.Book{
+		Title:  "Never Finished",
+		Author: "David Goggins",
+		Copies: 2,
+		ID:     "ABC03",
+	}
+
+	got, ok := books.GetBook("ABC03")
+	if !ok {
+		t.Fatal("Book not found")
+	}
+
+	if want != got {
+		t.Fatalf("want: %#v, got: %#v ", want, got)
+	}
+}
+
+func TestGetBook_ReturnsFalseWhenBookNotFound(t *testing.T) {
+	t.Parallel()
+
+	_, ok := books.GetBook("nonexistent ID")
+	if ok {
+		t.Fatal("want false for nonexistent ID, got true")
+	}
+}
+
+func TestAddBook_AddGivenBookToCatalog(t *testing.T) {
+	t.Parallel()
+
+	// Precondition
+	_, ok := books.GetBook("ABC03")
+	if ok {
+		t.Fatal("Book already present")
+	}
+
+	// Action
+	books.AddBook(books.Book{
+		ID:     "ABC03",
+		Title:  "Never Finished",
+		Author: "David Goggins",
+		Copies: 2,
+	})
+
+	// Postcondition
+	// _, ok := books.GetBook("ABC03")
+	if !ok {
+		t.Fatalf("Added book not found")
+	}
+
+}
