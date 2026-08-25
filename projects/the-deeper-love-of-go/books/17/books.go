845c292a3d7cafd0c247e53d27429d74b2c18008
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-08-25 14:50:50 +0700

    intial commit for learn defer keyword concept

---
+++ projects/the-deeper-love-of-go/books/17/books.go
@@ -0,0 +1,68 @@
+package books
+
+import (
+	"fmt"
+	"maps"
+	"slices"
+)
+
+type Book struct {
+	Title  string
+	Author string
+	Copies int
+	ID     string
+}
+
+type Catalog map[string]Book
+
+func (catalog Catalog) GetAllBooks() []Book {
+	return slices.Collect(maps.Values(catalog))
+}
+
+func (book Book) String() string {
+	result := fmt.Sprintf("%v by %v (copies: %v)", book.Title, book.Author, book.Copies)
+	return result
+}
+
+func (catalog Catalog) GetBook(ID string) (Book, bool) {
+	book, ok := catalog[ID]
+
+	return book, ok
+}
+
+func (catalog Catalog) AddBook(book Book) {
+	catalog[book.ID] = book
+}
+
+func (book *Book) SetCopies(copies int) error {
+	// Below, is a shallow copy: not exactly updating the book.Copies
+	// fmt.Println("Before update book.Copies = ", book.Copies)
+	// book.Copies = copies
+	// fmt.Println("After update book.Copies = ", book.Copies)
+
+	if copies < 0 {
+		return fmt.Errorf("negative number of copies: %d", copies)
+	}
+
+	book.Copies = copies
+
+	return nil
+
+}
+
+func GetCatalog() Catalog {
+	return Catalog{
+		"ABC03": {
+			Title:  "Never Finished",
+			Author: "David Goggins",
+			Copies: 2,
+			ID:     "ABC03",
+		},
+		"ABC04": {
+			Title:  "The Mountain is You",
+			Author: "Briana Weist",
+			Copies: 1,
+			ID:     "ABC04",
+		},
+	}
+}
---
+++ projects/the-deeper-love-of-go/books/17/cmd/find/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"books"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// os.Args is a slice of strings, came from user input as an arguments then store it
+	// os.Args[0]: Contains the name or path of the running program executable.
+	// os.Args[1:]: Contains all the actual user-provided arguments passed to the program.
+
+	// HOW TO USE:
+	// Current Folder -> go run main.go [bookId]
+	// root -> go run cmd/find [bookId]
+
+	catalog := books.GetCatalog()
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: find <BOOK ID>")
+		return
+	}
+
+	ID := os.Args[1]
+
+	book, ok := catalog.GetBook(ID)
+	if !ok {
+		fmt.Println("Sorry, Couldn't find that book in the catalog")
+		return
+	}
+
+	fmt.Println(book)
+
+}
