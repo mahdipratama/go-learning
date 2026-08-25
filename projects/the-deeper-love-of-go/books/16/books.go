08c8daf767b262486c7315ef65610e0cf67c5cb3
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-08-25 11:41:24 +0700

    initial commit for pointers chapter

---
+++ projects/the-deeper-love-of-go/books/16/books.go
@@ -0,0 +1,52 @@
+package books
+
+import (
+	"fmt"
+	"maps"
+	"slices"
+)
+
+type Book struct {
+	Title  string
+	Author string
+	Copies int
+	ID     string
+}
+
+type Catalog map[string]Book
+
+func (catalog Catalog) GetAllBooks() []Book {
+	return slices.Collect(maps.Values(catalog))
+}
+
+func (book Book) String() string {
+	result := fmt.Sprintf("%v by %v (copies: %v)", book.Title, book.Author, book.Copies)
+	return result
+}
+
+func (catalog Catalog) GetBook(ID string) (Book, bool) {
+	book, ok := catalog[ID]
+
+	return book, ok
+}
+
+func (catalog Catalog) AddBook(book Book) {
+	catalog[book.ID] = book
+}
+
+func GetCatalog() Catalog {
+	return Catalog{
+		"ABC03": {
+			Title:  "Never Finished",
+			Author: "David Goggins",
+			Copies: 2,
+			ID:     "ABC03",
+		},
+		"ABC04": {
+			Title:  "The Mountain is You",
+			Author: "Briana Weist",
+			Copies: 1,
+			ID:     "ABC04",
+		},
+	}
+}
---
+++ projects/the-deeper-love-of-go/books/16/books_test.go
@@ -0,0 +1,114 @@
+package books_test
+
+import (
+	"books"
+	"cmp"
+	"slices"
+	"testing"
+)
+
+func TestGetAllBooks_ReturnsAllBooks(t *testing.T) {
+	catalog := getTestCatalog()
+
+	want := []books.Book{
+		{
+			Title:  "The Mountain is You",
+			Author: "Briana Weist",
+			Copies: 1,
+			ID:     "ABC04",
+		},
+		{
+			Title:  "Never Finished",
+			Author: "David Goggins",
+			Copies: 2,
+			ID:     "ABC03",
+		},
+	}
+
+	got := catalog.GetAllBooks()
+	slices.SortFunc(got, func(a, b books.Book) int {
+		return cmp.Compare(a.Author, b.Author)
+	})
+
+	if !slices.Equal(want, got) {
+		t.Fatalf("want %#v, got %#v", want, got)
+	}
+}
+
+func TestGetBook_FindsBookInCatalogByID(t *testing.T) {
+	// Running all the test at the same time
+	t.Parallel()
+
+	catalog := getTestCatalog()
+
+	want := books.Book{
+		Title:  "Never Finished",
+		Author: "David Goggins",
+		Copies: 2,
+		ID:     "ABC03",
+	}
+
+	got, ok := catalog.GetBook("ABC03")
+	if !ok {
+		t.Fatal("Book not found")
+	}
+
+	if want != got {
+		t.Fatalf("want: %#v, got: %#v ", want, got)
+	}
+}
+
+func TestGetBook_ReturnsFalseWhenBookNotFound(t *testing.T) {
+	t.Parallel()
+
+	catalog := getTestCatalog()
+
+	_, ok := catalog.GetBook("nonexistent ID")
+	if ok {
+		t.Fatal("want false for nonexistent ID, got true")
+	}
+}
+
+func TestAddBook_AddGivenBookToCatalog(t *testing.T) {
+	t.Parallel()
+
+	catalog := getTestCatalog()
+
+	// Precondition
+	_, ok := catalog.GetBook("ABC05")
+	if ok {
+		t.Fatal("Book already present")
+	}
+
+	// Action
+	catalog.AddBook(books.Book{
+		ID:     "ABC05",
+		Title:  "Atomic Habit",
+		Author: "James Clear",
+		Copies: 2,
+	})
+
+	// Postcondition
+	_, ok = catalog.GetBook("ABC05")
+	if !ok {
+		t.Fatalf("Added book not found")
+	}
+
+}
+
+func getTestCatalog() books.Catalog {
+	return books.Catalog{
+		"ABC03": {
+			Title:  "Never Finished",
+			Author: "David Goggins",
+			Copies: 2,
+			ID:     "ABC03",
+		},
+		"ABC04": {
+			Title:  "The Mountain is You",
+			Author: "Briana Weist",
+			Copies: 1,
+			ID:     "ABC04",
+		},
+	}
+}
