cd59d076499edf050a593384e1e433e716732a80
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-08-31 11:38:06 +0700

    simulate data-race problems in goroutines

---
+++ projects/the-deeper-love-of-go/copies/1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	copies := 1
+
+	go func() {
+		if copies > 0 {
+			copies--
+			fmt.Println("Customer A got the book")
+		}
+	}()
+
+	go func() {
+		if copies > 0 {
+			copies--
+			fmt.Println("Customer B got the book")
+		}
+	}()
+
+	time.Sleep(time.Second)
+}
