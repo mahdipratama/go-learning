22817f558848e9594d1867f8e89488bc9569f28e
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-09-03 14:15:03 +0700

    implement bookstore API server

--- projects/the-deeper-love-of-go/books/19/server.go
+++ projects/the-deeper-love-of-go/books/19/server.go
@@ -1,5 +1,18 @@
 package books
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 func ListenAndServe(addr string, catalog *Catalog) error {
-	return nil
+	return http.ListenAndServe(addr, http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			books := catalog.GetAllBooks()
+			err := json.NewEncoder(w).Encode(books)
+			if err != nil {
+				panic(err)
+			}
+		}))
+
 }
