6bd587a158c379b43a40c1d9c90506ca79f2d7a3
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-09-04 10:57:47 +0700

    add: handlefunc to handle find books by id URI

--- projects/the-deeper-love-of-go/books/19/server.go
+++ projects/the-deeper-love-of-go/books/19/server.go
@@ -7,10 +7,22 @@ import (
 
 func ListenAndServe(addr string, catalog *Catalog) error {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/list",
 		func(w http.ResponseWriter, r *http.Request) {
 			books := catalog.GetAllBooks()
 			err := json.NewEncoder(w).Encode(books)
+			if err != nil {
+				panic(err)
+			}
+		})
+
+	mux.HandleFunc("/find/{ID}",
+		func(w http.ResponseWriter, r *http.Request) {
+			ID := r.PathValue("ID")
+			book, _ := catalog.GetBook(ID)
+
+			err := json.NewEncoder(w).Encode(book)
 			if err != nil {
 				panic(err)
 			}
