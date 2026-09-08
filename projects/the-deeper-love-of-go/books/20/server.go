4c47e22e271e95567f72e7a8be502f2b4b1d43d4
M.Mahdi Pratama Putra <108867144+mahdipratama@users.noreply.github.com>
2026-09-08 13:19:56 +0700

    add: On Client AddCopies handler request

--- projects/the-deeper-love-of-go/books/20/server.go
+++ projects/the-deeper-love-of-go/books/20/server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func ListenAndServe(addr string, catalog *Catalog) error {
@@ -44,6 +45,32 @@ func ListenAndServe(addr string, catalog *Catalog) error {
 			}
 
 			err = json.NewEncoder(w).Encode(copies)
+			if err != nil {
+				panic(err)
+			}
+		})
+
+	mux.HandleFunc("/v1/addcopies/{id}/{copies}",
+		func(w http.ResponseWriter, r *http.Request) {
+			ID := r.PathValue("id")
+			copies, err := strconv.Atoi(r.PathValue("copies"))
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			stock, err := catalog.AddCopies(ID, copies)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusNotFound)
+				return
+			}
+
+			err = catalog.Sync()
+			if err != nil {
+				panic(err)
+			}
+
+			err = json.NewEncoder(w).Encode(stock)
 			if err != nil {
 				panic(err)
 			}
